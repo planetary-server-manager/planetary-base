96a7d1a9eefc702f54cc97befc785515844abd88
renegadespork <jessekilburn@gmail.com>
2025-01-09 10:57:14 -0800

    Updated info.sh

--- info.sh
+++ info.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cat logo.txt && \
-echo "\nThis is a container image for the Planetary Server Manager. Game servers are downloaded, configured and run from here."
+echo "This is a container image for the Planetary Server Manager. Game servers are downloaded, configured and run from here."
